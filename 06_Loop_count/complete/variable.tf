c71ab01e3dc72972c75fe7e053079b237e8ff337
jeawung2 <130008456+jeawung2@users.noreply.github.com>
2024-10-16 19:35:23 +0900

    Create variable.tf

---
+++ 06_Loop_count/complete/variable.tf
@@ -0,0 +1,63 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "myproject"
+}
+
+variable "project_environment" {
+  description = "Name of the environment"
+  type        = string
+  default     = "development"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR Block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "Public Subnets for VPC"
+  type        = list(string)
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24",
+    "10.0.4.0/24"
+  ]
+}
+
+variable "private_subnets" {
+  description = "Private Subnets for VPC"
+  type        = list(string)
+  default = [
+    "10.0.101.0/24",
+    "10.0.102.0/24",
+    "10.0.103.0/24",
+    "10.0.104.0/24"
+  ]
+}
+
+variable "number_of_instances_per_subnet" {
+  description = "Number of Instances per Subnet"
+  type        = number
+  default     = 1
+}
+
+variable "number_of_subnets" {
+  description = "Number of Subnets"
+  type        = number
+  default     = 2
+}
